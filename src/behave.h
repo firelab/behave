0c043afc3c300ba3cff7cac3d14fc6e3d0bd03eb
WChatham <wmchatham79@gmail.com>
2016-02-24 11:43:13 -0700

    Fixed text alignment issues (hopefully)

--- src/behave.h
+++ src/behave.h
@@ -13,53 +13,51 @@
 class Behave
 {
 public:
-
-	
-	Behave();
-	explicit Behave(SurfaceInputs &surfaceInputs);
-	~Behave();
-	
-	// SURFACE Module Inputs 
-	void updateSurfaceInputs(int fuelModelNumber, double moistureOneHour, double moistureTenHour, double moistureHundredHour, 
-		double moistureLiveHerbaceous, double moistureLiveWoody, WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode, 
-		double windSpeed, double windDirection, double slope, double slopeAspect);
+    Behave();
+    explicit Behave(SurfaceInputs &surfaceInputs);
+    ~Behave();
+    
+    // SURFACE Module Inputs 
+    void updateSurfaceInputs(int fuelModelNumber, double moistureOneHour, double moistureTenHour, double moistureHundredHour, 
+        double moistureLiveHerbaceous, double moistureLiveWoody, WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode, 
+        double windSpeed, double windDirection, double slope, double slopeAspect);
 	void updateSurfaceInputsForTwoFuelModels(int firstfuelModelNumber, int secondFuelModelNumber,
-		double moistureOneHour, double moistureTenHour, double moistureHundredHour,
-		double moistureLiveHerbaceous, double moistureLiveWoody, WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode, 
-		double windSpeed,double windDirection, double firstFuelModelCoverage, TwoFuelModelsMethod::TwoFuelModelsMethodEnum method, 
-		double slope, double slopeAspect);
-	void  updateSurfaceInputsForPalmettoGallbery(double moistureOneHour, double moistureTenHour,double moistureHundredHour, 
-		double moistureLiveHerbaceous, double moistureLiveWoody, WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode, 
-		double windSpeed, double windDirection, double ageOfRough, double heightOfUnderstory,double palmettoCoverage, 
-		double overstoryBasalArea, double slope, double slopeAspect);
-	double calculateSurfaceFireForwardSpreadRate(double directionOfInterest = -1.0);
+        double moistureOneHour, double moistureTenHour, double moistureHundredHour,
+        double moistureLiveHerbaceous, double moistureLiveWoody, WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode, 
+        double windSpeed,double windDirection, double firstFuelModelCoverage, TwoFuelModelsMethod::TwoFuelModelsMethodEnum method, 
+        double slope, double slopeAspect);
+    void  updateSurfaceInputsForPalmettoGallbery(double moistureOneHour, double moistureTenHour,double moistureHundredHour, 
+        double moistureLiveHerbaceous, double moistureLiveWoody, WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode, 
+        double windSpeed, double windDirection, double ageOfRough, double heightOfUnderstory,double palmettoCoverage, 
+        double overstoryBasalArea, double slope, double slopeAspect);
+    double calculateSurfaceFireForwardSpreadRate(double directionOfInterest = -1.0);
 
-	// SURFACE Module Getters
-	double getDirectionOfMaxSpread() const;
-	double getFlameLength() const;
-	double getFireLengthToWidthRatio() const;
-	double getFireEccentricity() const;
-	bool isFuelModelDefined(int fuelModelNumber) const;
-	bool isWindAndSpreadAngleRelativeToNorth() const;
-	bool isWindAndSpreadAngleRelativeToUpslope() const;
-	bool isSlopeInDegrees() const;
-	bool isSlopeInPercent() const;
-
-	// SURFACE Module Setters
-	void setWindAndSpreadAnglesRelativeToNorth();
-	void setWindAndSpreadAnglesRelativeToUpslope();
-	void setSlopeInputToPercent();
-	void setSlopeInputToDegrees();
+    // SURFACE Module Getters
+    double getDirectionOfMaxSpread() const;
+    double getFlameLength() const;
+    double getFireLengthToWidthRatio() const;
+    double getFireEccentricity() const;
+    bool isFuelModelDefined(int fuelModelNumber) const;
+    bool isWindAndSpreadAngleRelativeToNorth() const;
+    bool isWindAndSpreadAngleRelativeToUpslope() const;
+    bool isSlopeInDegrees() const;
+    bool isSlopeInPercent() const;
+    
+    // SURFACE Module Setters
+    void setWindAndSpreadAnglesRelativeToNorth();
+    void setWindAndSpreadAnglesRelativeToUpslope();
+    void setSlopeInputToPercent();
+    void setSlopeInputToDegrees();
 	
 private:
-	// SURFACE Module Private Setters
-	void setSlopeInputMode(SlopeInputMode::SlopeInputModeEnum slopeInputMode);
-	void setWindAndSpreadDirectionMode(WindAndSpreadAngleMode::WindAndSpreadAngleModeEnum windAndSpreadAngleMode);
-
-	// SURFACE Module Component Objects
-	FuelModels fuelModels_;         // Object containing data for fuel models
-	Surface surface_;               // SURFACE Moduel object
-	SurfaceInputs surfaceInputs_;   // Object that manages user input to SURFACE Module
+    // SURFACE Module Private Setters
+    void setSlopeInputMode(SlopeInputMode::SlopeInputModeEnum slopeInputMode);
+    void setWindAndSpreadDirectionMode(WindAndSpreadAngleMode::WindAndSpreadAngleModeEnum windAndSpreadAngleMode);
+    
+    // SURFACE Module Component Objects
+    FuelModels fuelModels_;         // Object containing data for fuel models
+    Surface surface_;               // SURFACE Moduel object
+    SurfaceInputs surfaceInputs_;   // Object that manages user input to SURFACE Module
 };
 
 #endif //BEHAVE_HEADER
--- src/surfaceInputs.h
+++ src/surfaceInputs.h
@@ -8,59 +8,56 @@
 class SurfaceInputs
 {
 public:
-	SurfaceInputs();
+    SurfaceInputs();
 
-	void initializeMembers();
-	void updateInput(int fuelModelNumber, double moistureOneHour, double moistureTenHour, double moistureHundredHour, double moistureLiveHerbaceous, 
-		double moistureLiveWoody, WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode, double windSpeed, double windDirection, 
-		double slope, double slopeAspect);
-	void updateSurfaceInputsForTwoFuelModels(int firstfuelModelNumber, int secondFuelModelNumber,
-		double moistureOneHour, double moistureTenHour, double moistureHundredHour, double moistureLiveHerbaceous, double moistureLiveWoody,
-		WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode, double windSpeed, double windDirection, double firstFuelModelCoverage, 
-		TwoFuelModelsMethod::TwoFuelModelsMethodEnum twoFuelModelsMethod, double slope, double slopeAspect);
-	void  updateSurfaceInputsForPalmettoGallbery(double moistureOneHour, double moistureTenHour,
-		double moistureHundredHour, double moistureLiveHerbaceous, double moistureLiveWoody, WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode,
-		double windSpeed, double windDirection, double ageOfRough, double heightOfUnderstory,
-		double palmettoCoverage, double overstoryBasalArea, double slope, double slopeAspect);
-	void setFuelModelNumber(int fuelModelNumber);
-	void setMoistureDead(double moistureOneHour, double moistureTenHour, double moistureHundredHour);
-	void setMoistureLive(double moistureLiveHerbaceous, double moistureLiveWoody);
-	void setMoistureOneHour(double moistureOneHour);
-	void setMoistureTenHour(double moistureTenHour);
-	void setMoistureHundredHour(double moistureHundredHour);
-	void setMoistureLiveHerbaceous(double moistureLiveHerbaceous);
-	void setMoistureLiveWoody(double moistureLiveWoody);
-	void setSlope(double slope);
-	void setSlopeAspect(double slopeAspect);
-	void setSlopeInputMode(SlopeInputMode::SlopeInputModeEnum slopeInputMode);
-	void setMidflameWindSpeed(double midflameWindSpeed);
-	void setWindDirection(double windDirection);
-	void setWindAndSpreadAngleMode(WindAndSpreadAngleMode::WindAndSpreadAngleModeEnum windAndSpreadAngleMode);
-	void setFirstFuelModelNumber(int firstFuelModelNumber);
-	void setSecondFuelModelNumber(int secondFuelModelNumber);
-	void setTwoFuelModelsMethod(TwoFuelModelsMethod::TwoFuelModelsMethodEnum twoFuelModelsMethod);
-	
-	void setIsUsingPalmettoGallberry(bool isUsingPalmettoGallberry);
-	bool isUsingPalmettoGallberry() const;
+    void initializeMembers();
+    void updateInput(int fuelModelNumber, double moistureOneHour, double moistureTenHour, double moistureHundredHour, double moistureLiveHerbaceous, 
+        double moistureLiveWoody, WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode, double windSpeed, double windDirection, 
+        double slope, double slopeAspect);
+    void updateSurfaceInputsForTwoFuelModels(int firstfuelModelNumber, int secondFuelModelNumber,double moistureOneHour, double moistureTenHour, 
+        double moistureHundredHour, double moistureLiveHerbaceous, double moistureLiveWoody, WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode,
+        double windSpeed, double windDirection, double firstFuelModelCoverage, TwoFuelModelsMethod::TwoFuelModelsMethodEnum twoFuelModelsMethod, double slope, double slopeAspect);
+    void updateSurfaceInputsForPalmettoGallbery(double moistureOneHour, double moistureTenHour, double moistureHundredHour, double moistureLiveHerbaceous, double moistureLiveWoody, 
+        WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode, double windSpeed, double windDirection, double ageOfRough, double heightOfUnderstory,
+        double palmettoCoverage, double overstoryBasalArea, double slope, double slopeAspect);
+    void setFuelModelNumber(int fuelModelNumber);
+    void setMoistureDead(double moistureOneHour, double moistureTenHour, double moistureHundredHour);
+    void setMoistureLive(double moistureLiveHerbaceous, double moistureLiveWoody);
+    void setMoistureOneHour(double moistureOneHour);
+    void setMoistureTenHour(double moistureTenHour);
+    void setMoistureHundredHour(double moistureHundredHour);
+    void setMoistureLiveHerbaceous(double moistureLiveHerbaceous);
+    void setMoistureLiveWoody(double moistureLiveWoody);
+    void setSlope(double slope);
+    void setSlopeAspect(double slopeAspect);
+    void setSlopeInputMode(SlopeInputMode::SlopeInputModeEnum slopeInputMode);
+    void setMidflameWindSpeed(double midflameWindSpeed);
+    void setWindDirection(double windDirection);
+    void setWindAndSpreadAngleMode(WindAndSpreadAngleMode::WindAndSpreadAngleModeEnum windAndSpreadAngleMode);
+    void setFirstFuelModelNumber(int firstFuelModelNumber);
+    void setSecondFuelModelNumber(int secondFuelModelNumber);
+    void setTwoFuelModelsMethod(TwoFuelModelsMethod::TwoFuelModelsMethodEnum twoFuelModelsMethod);
 
-	void setAgeOfRough(double ageOfRough);
-	double getAgeOfRough() const;
-	void setHeightOfUnderstory(double heightOfUnderstory);
-	double getHeightOfUnderstory() const;
-	void setPalmettoCoverage(double palmettoCoverage);
-	double getPalmettoCoverage() const;
-	void setOverstoryBasalArea(double overstoryBasalArea);
-	double getOverstoryBasalArea() const;
+    void setIsUsingPalmettoGallberry(bool isUsingPalmettoGallberry);
+    bool isUsingPalmettoGallberry() const;
+    void setAgeOfRough(double ageOfRough);
+    double getAgeOfRough() const;
+    void setHeightOfUnderstory(double heightOfUnderstory);
+    double getHeightOfUnderstory() const;
+    void setPalmettoCoverage(double palmettoCoverage);
+    double getPalmettoCoverage() const;
+    void setOverstoryBasalArea(double overstoryBasalArea);
+    double getOverstoryBasalArea() const;
 
-	int getFuelModelNumber() const;
-	int getFirstFuelModelNumber() const;
-	int getSecondFuelModelNumber() const;
-	int getTwoFuelModelsMethod() const;
-	bool isWindAndSpreadAngleRelativeToNorth() const;
-	bool isWindAndSpreadAngleRelativeToUpslope() const;
-	bool isSlopeInDegrees() const;
-	bool isSlopeInPercent() const;
-	bool isUsingTwoFuelModels() const;
+    int getFuelModelNumber() const;
+    int getFirstFuelModelNumber() const;
+    int getSecondFuelModelNumber() const;
+    int getTwoFuelModelsMethod() const;
+    bool isWindAndSpreadAngleRelativeToNorth() const;
+    bool isWindAndSpreadAngleRelativeToUpslope() const;
+    bool isSlopeInDegrees() const;
+    bool isSlopeInPercent() const;
+    bool isUsingTwoFuelModels() const;
     double getMoistureDeadAtIndex(int index) const;
     double getMoistureLiveAtIndex(int index) const;
     double getSlope() const;
@@ -73,44 +70,44 @@ public:
     double getMoistureLiveHerbaceous() const;
     double getMoistureLiveWoody() const;
     double getCoverage() const;
-	
+
 private:
-	double convertWindToUpslope(double windDirectionFromNorth);
-	void setMoistureDead();
-	void setMoistureLive();
+    double convertWindToUpslope(double windDirectionFromNorth);
+    void setMoistureDead();
+    void setMoistureLive();
 
-	static const int MAX_SIZES = 4;		// Max number of fuel size classes
-	
-	int     fuelModelNumber_;           // 1 to 256
-	double	moistureOneHour_;           // 1% to 60%
-	double	moistureTenHour_;           // 1% to 60%		
-	double	moistureHundredHour_;       // 1% to 60%
-    double	moistureLiveHerbaceous_;    // 30% to 300%
-	double	moistureLiveWoody_;         // 30% to 300%
-	double	midflameWindSpeed_;         // in miles per hour
-	double	windDirection_;             // degrees, 0-360
-	double	slope_;                     // gradient 0-600 or degrees 0-80  
-	double	slopeAspect_;               // degrees, 0-360
-    double	moistureDead_[MAX_SIZES];   // dead fuel moisture content
-    double	moistureLive_[MAX_SIZES];   // live fuel moisture content
-   
-	// Two Fuel Models
-    bool	isUsingTwoFuelModels_;      // Whether fuel spread calculation is using Two Fuel Models
-    int		firstFuelModelNumber_;      // 1 to 256, first fuel used in Two Fuel Models
-    int		secondFuelModelNumber_;     // 1 to 256, second fuel used in Two Fuel Models
-    double	firstFuelModelCoverage_;    // percent of landscape occupied by first fuel in Two Fuel Models
+    static const int MAX_SIZES = 4;     // Max number of fuel size classes
 
-	// Palmetto-Gallberry
-	bool    isUsingPalmettoGallberry_;  // Whether fuel spread calculation is using Palmetto-Gallbery
-	double  ageOfRough_;
-	double  heightOfUnderstory_;
-	double  palmettoCoverage_;
-	double  overstoryBasalArea_;
+    int fuelModelNumber_;               // 1 to 256
+    double moistureOneHour_;            // 1% to 60%
+    double moistureTenHour_;            // 1% to 60%		
+    double moistureHundredHour_;        // 1% to 60%
+    double moistureLiveHerbaceous_;     // 30% to 300%
+    double moistureLiveWoody_;          // 30% to 300%
+    double midflameWindSpeed_;          // in miles per hour
+    double windDirection_;              // degrees, 0-360
+    double slope_;                      // gradient 0-600 or degrees 0-80  
+    double slopeAspect_;                // degrees, 0-360
+    double moistureDead_[MAX_SIZES];    // dead fuel moisture content
+    double moistureLive_[MAX_SIZES];    // live fuel moisture content
 
-	SlopeInputMode::SlopeInputModeEnum slopeInputMode_;                         // Specifies whether slope is input as percent or degrees
-	TwoFuelModelsMethod::TwoFuelModelsMethodEnum twoFuelModelsMethod_;          // Specifies method used in Two Fuel Models calculations
-	WindAndSpreadAngleMode::WindAndSpreadAngleModeEnum windAndSpreadAngleMode_; // Specifies how wind and spread directions are referenced
-	WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode_;          // Specifies the height above canopy from which wind speed is measured
+    // Two Fuel Models
+    bool isUsingTwoFuelModels_;         // Whether fuel spread calculation is using Two Fuel Models
+    int firstFuelModelNumber_;          // 1 to 256, first fuel used in Two Fuel Models
+    int secondFuelModelNumber_;         // 1 to 256, second fuel used in Two Fuel Models
+    double firstFuelModelCoverage_;     // percent of landscape occupied by first fuel in Two Fuel Models
+
+    // Palmetto-Gallberry
+    bool    isUsingPalmettoGallberry_;  // Whether fuel spread calculation is using Palmetto-Gallbery
+    double  ageOfRough_;
+    double  heightOfUnderstory_;
+    double  palmettoCoverage_;
+    double  overstoryBasalArea_;
+
+    SlopeInputMode::SlopeInputModeEnum slopeInputMode_;                         // Specifies whether slope is input as percent or degrees
+    TwoFuelModelsMethod::TwoFuelModelsMethodEnum twoFuelModelsMethod_;          // Specifies method used in Two Fuel Models calculations
+    WindAndSpreadAngleMode::WindAndSpreadAngleModeEnum windAndSpreadAngleMode_; // Specifies how wind and spread directions are referenced
+    WindHeightInputMode::WindHeightInputModeEnum windHeightInputMode_;          // Specifies the height above canopy from which wind speed is measured
 };
 
 #endif // SURFACEINPUTS_HEADER
