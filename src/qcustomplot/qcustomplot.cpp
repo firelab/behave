429a701d1998ba97995084d482bd1938d84fec9b
WChatham <wmchatham79@gmail.com>
2016-04-25 16:30:39 -0600

    Uncommented QCustomPlot::savePdf()

--- src/qcustomplot/qcustomplot.cpp
+++ src/qcustomplot/qcustomplot.cpp
@@ -10446,68 +10446,68 @@ void QCustomPlot::rescaleAxes(bool onlyVisiblePlottables)
   
   \see savePng, saveBmp, saveJpg, saveRastered
 */
-//bool QCustomPlot::savePdf(const QString &fileName, bool noCosmeticPen, int width, int height, const QString &pdfCreator, const QString &pdfTitle)
-//{
-//  bool success = false;
-//#ifdef QT_NO_PRINTER
-//  Q_UNUSED(fileName)
-//  Q_UNUSED(noCosmeticPen)
-//  Q_UNUSED(width)
-//  Q_UNUSED(height)
-//  Q_UNUSED(pdfCreator)
-//  Q_UNUSED(pdfTitle)
-//  qDebug() << Q_FUNC_INFO << "Qt was built without printer support (QT_NO_PRINTER). PDF not created.";
-//#else
-//  int newWidth, newHeight;
-//  if (width == 0 || height == 0)
-//  {
-//    newWidth = this->width();
-//    newHeight = this->height();
-//  } else
-//  {
-//    newWidth = width;
-//    newHeight = height;
-//  }
-//  
-//  QPrinter printer(QPrinter::ScreenResolution);
-//  printer.setOutputFileName(fileName);
-//  printer.setOutputFormat(QPrinter::PdfFormat);
-//  printer.setColorMode(QPrinter::Color);
-//  printer.printEngine()->setProperty(QPrintEngine::PPK_Creator, pdfCreator);
-//  printer.printEngine()->setProperty(QPrintEngine::PPK_DocumentName, pdfTitle);
-//  QRect oldViewport = viewport();
-//  setViewport(QRect(0, 0, newWidth, newHeight));
-//#if QT_VERSION < QT_VERSION_CHECK(5, 3, 0)
-//  printer.setFullPage(true);
-//  printer.setPaperSize(viewport().size(), QPrinter::DevicePixel);
-//#else
-//  QPageLayout pageLayout;
-//  pageLayout.setMode(QPageLayout::FullPageMode);
-//  pageLayout.setOrientation(QPageLayout::Portrait);
-//  pageLayout.setMargins(QMarginsF(0, 0, 0, 0));
-//  pageLayout.setPageSize(QPageSize(viewport().size(), QPageSize::Point, QString(), QPageSize::ExactMatch));
-//  printer.setPageLayout(pageLayout);
-//#endif
-//  QCPPainter printpainter;
-//  if (printpainter.begin(&printer))
-//  {
-//    printpainter.setMode(QCPPainter::pmVectorized);
-//    printpainter.setMode(QCPPainter::pmNoCaching);
-//    printpainter.setMode(QCPPainter::pmNonCosmetic, noCosmeticPen);
-//    printpainter.setWindow(mViewport);
-//    if (mBackgroundBrush.style() != Qt::NoBrush &&
-//        mBackgroundBrush.color() != Qt::white &&
-//        mBackgroundBrush.color() != Qt::transparent &&
-//        mBackgroundBrush.color().alpha() > 0) // draw pdf background color if not white/transparent
-//      printpainter.fillRect(viewport(), mBackgroundBrush);
-//    draw(&printpainter);
-//    printpainter.end();
-//    success = true;
-//  }
-//  setViewport(oldViewport);
-//#endif // QT_NO_PRINTER
-//  return success;
-//}
+bool QCustomPlot::savePdf(const QString &fileName, bool noCosmeticPen, int width, int height, const QString &pdfCreator, const QString &pdfTitle)
+{
+  bool success = false;
+#ifdef QT_NO_PRINTER
+  Q_UNUSED(fileName)
+  Q_UNUSED(noCosmeticPen)
+  Q_UNUSED(width)
+  Q_UNUSED(height)
+  Q_UNUSED(pdfCreator)
+  Q_UNUSED(pdfTitle)
+  qDebug() << Q_FUNC_INFO << "Qt was built without printer support (QT_NO_PRINTER). PDF not created.";
+#else
+  int newWidth, newHeight;
+  if (width == 0 || height == 0)
+  {
+    newWidth = this->width();
+    newHeight = this->height();
+  } else
+  {
+    newWidth = width;
+    newHeight = height;
+  }
+  
+  QPrinter printer(QPrinter::ScreenResolution);
+  printer.setOutputFileName(fileName);
+  printer.setOutputFormat(QPrinter::PdfFormat);
+  printer.setColorMode(QPrinter::Color);
+  printer.printEngine()->setProperty(QPrintEngine::PPK_Creator, pdfCreator);
+  printer.printEngine()->setProperty(QPrintEngine::PPK_DocumentName, pdfTitle);
+  QRect oldViewport = viewport();
+  setViewport(QRect(0, 0, newWidth, newHeight));
+#if QT_VERSION < QT_VERSION_CHECK(5, 3, 0)
+  printer.setFullPage(true);
+  printer.setPaperSize(viewport().size(), QPrinter::DevicePixel);
+#else
+  QPageLayout pageLayout;
+  pageLayout.setMode(QPageLayout::FullPageMode);
+  pageLayout.setOrientation(QPageLayout::Portrait);
+  pageLayout.setMargins(QMarginsF(0, 0, 0, 0));
+  pageLayout.setPageSize(QPageSize(viewport().size(), QPageSize::Point, QString(), QPageSize::ExactMatch));
+  printer.setPageLayout(pageLayout);
+#endif
+  QCPPainter printpainter;
+  if (printpainter.begin(&printer))
+  {
+    printpainter.setMode(QCPPainter::pmVectorized);
+    printpainter.setMode(QCPPainter::pmNoCaching);
+    printpainter.setMode(QCPPainter::pmNonCosmetic, noCosmeticPen);
+    printpainter.setWindow(mViewport);
+    if (mBackgroundBrush.style() != Qt::NoBrush &&
+        mBackgroundBrush.color() != Qt::white &&
+        mBackgroundBrush.color() != Qt::transparent &&
+        mBackgroundBrush.color().alpha() > 0) // draw pdf background color if not white/transparent
+      printpainter.fillRect(viewport(), mBackgroundBrush);
+    draw(&printpainter);
+    printpainter.end();
+    success = true;
+  }
+  setViewport(oldViewport);
+#endif // QT_NO_PRINTER
+  return success;
+}
 
 /*!
   Saves a PNG image file to \a fileName on disc. The output plot will have the dimensions \a width
--- src/qcustomplot/qcustomplot.h
+++ src/qcustomplot/qcustomplot.h
@@ -1807,7 +1807,7 @@ public:
   QList<QCPLegend*> selectedLegends() const;
   Q_SLOT void deselectAll();
   
-//  bool savePdf(const QString &fileName, bool noCosmeticPen=false, int width=0, int height=0, const QString &pdfCreator=QString(), const QString &pdfTitle=QString());
+  bool savePdf(const QString &fileName, bool noCosmeticPen=false, int width=0, int height=0, const QString &pdfCreator=QString(), const QString &pdfTitle=QString());
   bool savePng(const QString &fileName, int width=0, int height=0, double scale=1.0, int quality=-1);
   bool saveJpg(const QString &fileName, int width=0, int height=0, double scale=1.0, int quality=-1);
   bool saveBmp(const QString &fileName, int width=0, int height=0, double scale=1.0);
