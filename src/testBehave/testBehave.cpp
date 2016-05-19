1ad6d680fe7e58b303b714599e7e2832fe35df01
WChatham <wmchatham79@gmail.com>
2016-05-19 11:15:55 -0600

    Made first test case for testing a behave run

--- src/testBehave/testBehave.cpp
+++ src/testBehave/testBehave.cpp
@@ -5,32 +5,46 @@
 #include "fuelModelSet.h"
 #include "behaveRun.h"
 
-int add(int i, int j) { return i + j; }
+static const double ERROR_TOLERANCE = 1e-05;
 
-FuelModelSet fuelModelSet;
-BehaveRun behave(fuelModelSet);
-
-BOOST_AUTO_TEST_CASE(BehaveTest)
+struct SingleFuelModelTest
 {
-    // seven ways to detect and report the same error:
-    BOOST_CHECK(add(2, 2) == 4);        // #1 continues on error
+    FuelModelSet fuelModelSet;
+    BehaveRun behaveSingleFuelModelTest;
 
-    //BOOST_CHECK(behaveTest[0].calculateSurfaceFireForwardSpreadRate() == 0);
+    SingleFuelModelTest()
+        : behaveSingleFuelModelTest(fuelModelSet)
+    {
+        
 
-    BOOST_REQUIRE(add(2, 2) == 4);      // #2 throws on error
 
-    if (add(2, 2) != 4)
-        BOOST_ERROR("Ouch...");            // #3 continues on error
+    }
+};
 
-    if (add(2, 2) != 4)
-        BOOST_FAIL("Ouch...");             // #4 throws on error
+BOOST_FIXTURE_TEST_SUITE(BehaveTest, SingleFuelModelTest)
 
-    if (add(2, 2) != 4) throw "Ouch..."; // #5 throws on error
+BOOST_AUTO_TEST_CASE(lowMoistureContent)
+{
+    int fuelModelNumber = 1;
+    double moistureOneHour = 6.0;
+    double moistureTenHour = 7.0;
+    double moistureHundredHour = 8.0;
+    double moistureLiveHerbaceous = 60.0;
+    double moistureLiveWoody = 90.0;
+    double windSpeed = 5.0;
+    double windDirection = 0;
+    double slope = 30.0;
+    double aspect = 0;
+    double canopyCover = 0.0;
+    double canopyHeight = 0.0;
+    double crownRatio = 0.0;
 
-    BOOST_CHECK_MESSAGE(add(2, 2) == 4,  // #6 continues on error
-        "add(..) result: " << add(2, 2));
+    behaveSingleFuelModelTest.updateSurfaceInputs(fuelModelNumber, moistureOneHour, 
+        moistureTenHour, moistureHundredHour, moistureLiveHerbaceous, moistureLiveWoody, 
+        WindHeightInputMode::DIRECT_MIDFLAME, windSpeed, windDirection, slope, aspect, 
+        canopyCover, canopyHeight, crownRatio);
 
-    BOOST_CHECK_EQUAL(add(2, 2), 4);	  // #7 continues on error
+    BOOST_CHECK_CLOSE(behaveSingleFuelModelTest.calculateSurfaceFireForwardSpreadRate(), 109.394614, ERROR_TOLERANCE);
 
     // Make Visual Studio wait while in debug mode
 #ifndef NDEBUG
@@ -38,3 +52,5 @@ BOOST_AUTO_TEST_CASE(BehaveTest)
     std::cin.get();
 #endif
 }
+
+BOOST_AUTO_TEST_SUITE_END()
