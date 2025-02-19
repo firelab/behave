684970747f2e4a467c4c71c6c31a20369675e8a1
Kenneth Cheung <kcheung331@gmail.com>
2025-02-19 15:03:25 -0500

    update spcies master table

--- src/behave/species_master_table.h
+++ src/behave/species_master_table.h
@@ -1,7 +1,7 @@
 /*{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}
-* Name: species_master_table.h
-* Desc:
-*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}*/
+ * Name: species_master_table.h
+ * Desc:
+ *}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}{*}*/
 
 #ifndef SPECIES_MASTER_TABLE_H
 #define SPECIES_MASTER_TABLE_H
@@ -16,12 +16,12 @@ using std::string;
 using std::vector;
 
 enum class RegionCode
-{
-    interior_west = 1,
-    pacific_west = 2,
-    north_east = 3,
-    south_east = 4
-};
+    {
+        interior_west = 1,
+        pacific_west = 2,
+        north_east = 3,
+        south_east = 4
+    };
 
 /*.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.- */
 /*                          Species Master Table Record                      */
@@ -33,10 +33,15 @@ struct SpeciesMasterTableRecord
     int  mortalityEquationNumber = -1;
     int  barkEquationNumber = -1;
     int  crownCoefficientCode; /* canopy cover equation #, (FVS Species Index No. )      */
-    int8_t  regionInteriorWest = false; // Region 1
-    int8_t  regionPacificWest = false; // Region 2
-    int8_t  regionNorthEast = false; // Region 3
-    int8_t  regionSouthEast = false;  // Region 4
+    int8_t gaccAlaska = false;
+    int8_t gaccCalifornia = false;
+    int8_t gaccEasternArea = false;
+    int8_t gaccGreatBasin = false;
+    int8_t gaccNorthernRockies = false;
+    int8_t gaccNorthwest = false;
+    int8_t gaccRocketyMountain = false;
+    int8_t gaccSouthernArea = false;
+    int8_t gaccSouthWest = false;
     EquationType equationType = EquationType::not_set;
     CrownDamageEquationCode crownDamageEquationCode = CrownDamageEquationCode::not_set;
 };
@@ -49,18 +54,31 @@ public:
     ~SpeciesMasterTable();
 
     void  initializeMasterTable();
-  
+
     int getSpeciesTableIndexFromSpeciesCode(string speciesCode) const;
     int getSpeciesTableIndexFromSpeciesCodeAndEquationType(string speciesCode, EquationType equationType) const;
-   
+
     vector<SpeciesMasterTableRecord> record_;
 
     CanopyCoefficientTable canopyCoefficientTable;
 
-    void insertRecord(string speciesCode, string scientificName, string commonName,
-        int  mortalityEquation, int  brkEqu, int  crownCoefficientCode,
-        int8_t region1, int8_t  region2, int8_t  region3, int8_t region4, EquationType equationType,
-        CrownDamageEquationCode crownDamageEquationCode);
+    void insertRecord (string speciesCode,
+                       string scientificName,
+                       string commonName,
+                       int  mortalityEquation,
+                       int  brkEqu,
+                       int  crownCoefficientCode,
+                       int8_t gaccAlaska,
+                       int8_t gaccCalifornia,
+                       int8_t gaccEasternArea,
+                       int8_t gaccGreatBasin,
+                       int8_t gaccNorthernRockies,
+                       int8_t gaccNorthwest,
+                       int8_t gaccRocketyMountain,
+                       int8_t gaccSouthernArea,
+                       int8_t gaccSouthWest,
+                       EquationType equationType,
+                       CrownDamageEquationCode crownDamageEquationCode);
 };
 
 #endif // SPECIES_MASTER_TABLE_H
